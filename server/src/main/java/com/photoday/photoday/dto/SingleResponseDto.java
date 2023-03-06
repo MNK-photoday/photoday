21bdabeced96d3fa72a95172932a251b17f02be4
youngrong <whdudfhd1127@naver.com>
2023-03-07 00:26:28 +0900

    [fix] 수정

--- server/src/main/java/com/photoday/photoday/dto/SingleResponseDto.java
+++ server/src/main/java/com/photoday/photoday/dto/SingleResponseDto.java
@@ -1,10 +1,12 @@
 package com.photoday.photoday.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
 public class SingleResponseDto<T> {
     T data;
 }
