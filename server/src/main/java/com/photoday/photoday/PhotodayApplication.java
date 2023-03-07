376a65700fe0c05771a4d352a7b32728f8b8af9d
heyroocoke <heyroocoke@gmail.com>
2023-03-07 14:44:34 +0900

    🎉 [Init] spring begin

---
+++ server/src/main/java/com/photoday/photoday/PhotodayApplication.java
@@ -0,0 +1,13 @@
+package com.photoday.photoday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotodayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotodayApplication.class, args);
+	}
+
+}
