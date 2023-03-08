9ea5433794d4777a1defd7fcd85749adefd1fae1
youa7878 <dkgus7123@naver.com>
2023-03-08 10:41:10 +0900

    ✨ [Feat] Route 설정 #10

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,32 @@
-import GlobalStyle from './Styles/GlobalStyles';
+// import GlobalStyle from './Styles/GlobalStyles';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Root, LoginRoot } from './routes/Route';
+import Main from './pages/Main/Main';
+import MyPage from './pages/MyPage/MyPage';
+import Login from './pages/Login/Login';
 
 function App() {
-  return (
-    <>
-      <GlobalStyle />
-    </>
-  );
+  const router = createBrowserRouter([
+    {
+      path: '/', // Header, Footer가 있는 페이지
+      element: <Root />, // 템플릿
+      children: [
+        { path: '/', element: <Main /> },
+        // { element: <Main /> },
+        {
+          path: '/mypage',
+          element: <MyPage />,
+        },
+      ],
+    },
+    {
+      path: '/login',
+      element: <LoginRoot />,
+      children: [{ path: '/login', element: <Login /> }],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
