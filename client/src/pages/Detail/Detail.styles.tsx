ddec7aa9410b8bdb558884f37a2afae152915340
heyroocoke <heyroocoke@gmail.com>
2023-03-10 07:09:08 +0900

    💄 [Design] Detail Page 레이아웃 구현 작업 #31

---
+++ client/src/pages/Detail/Detail.styles.tsx
@@ -0,0 +1,61 @@
+import exp from 'constants';
+import styled from 'styled-components';
+import { RowFlex, ColFlex } from '../../styles/GlobalStyles';
+
+export const DetailBox = styled.div`
+  ${ColFlex}
+  align-items: center;
+  justify-content: flex-start;
+
+  padding-top: 40px;
+
+  background-color: skyblue;
+
+  height: auto;
+`;
+
+export const PicBox = styled.div`
+  ${ColFlex}
+  justify-content: space-between;
+  align-items: center;
+
+  width: 100%;
+  height: auto;
+  background-color: aliceblue;
+`;
+
+export const ContentsTop = styled.div`
+  ${RowFlex}
+  width: 100%;
+  justify-content: space-between;
+`;
+export const Contents = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 85%;
+  height: auto;
+
+  > img {
+    width: 100%;
+    height: auto;
+  }
+`;
+export const ContentsBottom = styled.div`
+  ${RowFlex}
+  width: 100%;
+  justify-content: space-between;
+`;
+
+export const TagBox = styled.div`
+  ${RowFlex}
+  width: 85%;
+  height: auto;
+
+  background-color: #ccc;
+`;
+
+export const SeachList = styled.div`
+  width: 85%;
+  height: 1000px;
+`;
