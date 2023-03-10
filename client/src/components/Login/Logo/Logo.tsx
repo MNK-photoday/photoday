24b913105ffee1d7a4e6d51fdbad85c2c2f079ca
youa7878 <dkgus7123@naver.com>
2023-03-10 11:11:12 +0900

    💄 [Design] Logo Component 구현 #19

---
+++ client/src/components/Login/Logo/Logo.tsx
@@ -0,0 +1,13 @@
+import * as style from './Logo.style';
+
+function Logo() {
+  return (
+    <style.LoginLogoContainer>
+      <style.Logo>
+        pho<style.LogoPoint>to</style.LogoPoint>day
+      </style.Logo>
+    </style.LoginLogoContainer>
+  );
+}
+
+export default Logo;
