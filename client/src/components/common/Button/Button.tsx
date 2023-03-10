27384d51e963d71ffa91e88192c8f336e9751ebb
18137036 <9wjdgh9@naver.com>
2023-03-10 15:49:11 +0900

    :lipstick: [Design] Button Component 구현 #42

--- client/src/components/common/Button/Button.tsx
+++ client/src/components/common/Button/Button.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { Children } from 'react';
+import * as style from './Button.style';
 
-function Button() {
-  return <div></div>;
+interface ButtonProps {
+  variant: 'primary' | 'point';
+  shape: 'default' | 'round';
+  size: 'small' | 'medium' | 'large';
+  disabled?: boolean;
+  fullWidth?: boolean;
+  children: React.ReactNode;
+}
+
+function Button({
+  variant,
+  shape,
+  size,
+  disabled = false,
+  fullWidth = false,
+  children,
+}: ButtonProps) {
+  return (
+    <style.ButtonBox
+      variant={variant}
+      shape={shape}
+      size={size}
+      fullWidth={fullWidth}
+      disabled={disabled}
+    >
+      {children}
+    </style.ButtonBox>
+  );
 }
 
 export default Button;
