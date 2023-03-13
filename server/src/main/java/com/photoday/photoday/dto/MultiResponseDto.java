6055544fc17ccb7e7ffa1fca2d494b7a27de6362
ichubtou <msjj0809@naver.com>
2023-03-13 15:30:52 +0900

    :sparkles: [Feat] 공통Dto 수정

--- server/src/main/java/com/photoday/photoday/dto/MultiResponseDto.java
+++ server/src/main/java/com/photoday/photoday/dto/MultiResponseDto.java
@@ -1,14 +1,33 @@
 package com.photoday.photoday.dto;
 
-import com.photoday.photoday.image.entity.Image;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 @Getter
-@AllArgsConstructor
 public class MultiResponseDto<T> {
     private List<T> data;
     private PageInfo pageInfo;
+
+    public MultiResponseDto(List<T> data, Page page) {
+        this.data = data;
+        this.pageInfo = new PageInfo(page.getNumber()+1, page.getSize(), page.getTotalPages(), page.getTotalElements());
+    }
+
+    @Getter
+    private class PageInfo{
+        private int pageNumber;
+        private int size;
+        private int totalPages;
+        private long totalElements;
+
+        public PageInfo(int pageNumber, int size, int totalPages, long totalElements) {
+            this.pageNumber = pageNumber;
+            this.size = size;
+            this.totalPages = totalPages;
+            this.totalElements = totalElements;
+        }
+    }
 }
