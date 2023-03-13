9ee8fad211abb3426b06cebcba35997734e86590
18137036 <9wjdgh9@naver.com>
2023-03-13 12:04:18 +0900

    :lipstick: [Design] Main Page 구현 #40]
    textBoxActive props 값으로 TextBox와 ImageBox 활성화/ 비활성화 작업

--- client/src/pages/Main/Main.tsx
+++ client/src/pages/Main/Main.tsx
@@ -1,34 +1,58 @@
 import { Container, ContainerWrap } from '../../styles/Layout';
-import * as style from './Main.styles';
-
-import image1 from '../../assets/imgs/image1.jpg';
+import MainImage from '../../assets/imgs/image1.jpg';
 import SearchBar from '../../components/common/SearchBar/SearchBar';
+import { useState } from 'react';
+import Card from '../../components/common/Card/Card';
+import {
+  S_ContentImgBox,
+  S_MainContent,
+  S_MainContentBox,
+  S_MainImg,
+  S_MainImgBox,
+  S_MainTextBox,
+  S_MainTextSpan,
+  S_SearchContentBox,
+  S_SearchImgBox,
+} from './Main.styles';
+
 function Main() {
+  const [activeTextBox, setActiveTextBox] = useState(true);
+
   return (
     <ContainerWrap>
       <Container>
-        <style.MainContentBox>
-          <style.MainContent>
-            <style.SearchContentBox>
-              <SearchBar />
-              <style.SearchImgBox></style.SearchImgBox>
-              <style.MainTextBox>
-                <style.MainTextSpan>
+        <S_MainContentBox>
+          <S_MainContent>
+            <S_SearchContentBox>
+              <SearchBar setActiveTextBox={setActiveTextBox} />
+              <S_SearchImgBox textBoxActive={activeTextBox}>
+                <S_ContentImgBox>
+                  <Card />
+                  <Card />
+                  <Card />
+                  <Card />
+                  <Card />
+                  <Card />
+                  <Card />
+                </S_ContentImgBox>
+              </S_SearchImgBox>
+              <S_MainTextBox textBoxActive={activeTextBox}>
+                <S_MainTextSpan>
                   free pics.
                   <br /> do anyhing (CC0). <br /> make magic
-                </style.MainTextSpan>
-              </style.MainTextBox>
-            </style.SearchContentBox>
-          </style.MainContent>
-          <style.MainContent>
-            <style.MainImgBox>
-              <style.MainImg src={image1}></style.MainImg>
-            </style.MainImgBox>
-            <style.MainImgBox>
-              <style.MainImg src={image1}></style.MainImg>
-            </style.MainImgBox>
-          </style.MainContent>
-        </style.MainContentBox>
+                </S_MainTextSpan>
+              </S_MainTextBox>
+            </S_SearchContentBox>
+          </S_MainContent>
+          <S_MainContent>
+            <S_MainImgBox>
+              <S_MainImg src={MainImage}></S_MainImg>
+            </S_MainImgBox>
+            <S_MainImgBox>
+              <S_MainImg src={MainImage}></S_MainImg>
+            </S_MainImgBox>
+          </S_MainContent>
+        </S_MainContentBox>
       </Container>
     </ContainerWrap>
   );
