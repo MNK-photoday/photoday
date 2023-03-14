65114fc96a41c79cbeaa4dfb8b4991c1a3a0d0f0
heyroocoke <heyroocoke@gmail.com>
2023-03-14 12:30:29 +0900

    🔨 [Feat] Choose file button Development #68

--- client/src/pages/Upload/Upload.tsx
+++ client/src/pages/Upload/Upload.tsx
@@ -12,9 +12,16 @@ import {
   S_UploadTitle,
 } from './Upload.styles';
 import { IoClose } from 'react-icons/io5';
+import { useRef } from 'react';
 
 function Upload() {
-  const InputTagHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const chooseFileRef = useRef<HTMLInputElement>(null);
+
+  const chooseFileHandler = () => {
+    chooseFileRef.current?.click();
+  };
+
+  const inputTagHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     console.log(event.target.value);
   };
   return (
@@ -23,12 +30,13 @@ function Upload() {
         <S_UploadBox>
           <S_UploadTitle>File Upload</S_UploadTitle>
           <S_FileBox>Drag files to upload</S_FileBox>
+          <input type="file" accept="image/*" ref={chooseFileRef} />
           <S_UploadBottom>
             <S_TagContainer>
               <S_TagInput
                 placeholder="add to tag"
                 type="text"
-                onChange={InputTagHandler}
+                onChange={inputTagHandler}
               ></S_TagInput>
               <S_TagBox>
                 <S_Tag>
@@ -37,7 +45,12 @@ function Upload() {
               </S_TagBox>
             </S_TagContainer>
             <S_ButtonContainer>
-              <Button variant={'primary'} shape={'round'} size={'medium'}>
+              <Button
+                variant={'primary'}
+                shape={'round'}
+                size={'medium'}
+                buttonClickEvent={chooseFileHandler}
+              >
                 Choose file
               </Button>
               <Button variant={'point'} shape={'round'} size={'medium'}>
