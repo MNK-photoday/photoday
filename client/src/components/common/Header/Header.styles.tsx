5225d3fd4376854aab34607fc91d204b5723dd28
18137036 <9wjdgh9@naver.com>
2023-03-14 14:36:28 +0900

    :lipstick: [Design] 로그인 상태 Header 구현 #32

--- client/src/components/common/Header/Header.styles.tsx
+++ client/src/components/common/Header/Header.styles.tsx
@@ -1,43 +1,76 @@
-import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import { ColFlex, RowFlex } from '../../../styles/GlobalStyles';
+import { Link } from 'react-router-dom';
+import { RowFlex } from '../../../styles/GlobalStyles';
 
-export const S_HeaderContainerWrap = styled.div`
+export const S_HeaderContainerWrap = styled.header`
   display: flex;
   justify-content: center;
 `;
-
 export const S_HeaderContainerFlex = styled.div`
   width: 100%;
   max-width: 90vw;
 `;
-
 export const S_HeaderContent = styled.div`
   ${RowFlex}
   height: 70px;
   align-items: center;
   justify-content: space-between;
 `;
-
 export const S_LogoBox = styled.div`
   margin-right: 100px;
+  height: 43px;
 `;
-export const S_Logo = styled.p`
+export const S_LogoH1 = styled.h1`
+  font-weight: normal;
   color: var(--color-primary-black);
-  font-size: var(--font-size-xl);
+  font-size: 27px;
 `;
-
 export const S_LogoPoint = styled.span`
-  font-size: var(--font-size-xl);
+  font-size: 27px;
   color: var(--color-primary-green);
 `;
-export const S_LinkBox = styled.div``;
+export const S_NavBox = styled.nav`
+  ${RowFlex}
+  align-items: center;
+  height: 100%;
 
-export const S_HeaderLink = styled(Link)`
+  .addImage-icon {
+    color: var(--color-primary-green);
+    font-size: 30px;
+    margin-top: 3px;
+  }
+`;
+export const S_NavLink = styled(Link)`
   color: var(--color-primary-black);
   font-size: var(--font-size-sm);
   margin-left: 30px;
   &:first-child {
     margin-left: 100px;
+  }
+`;
+export const S_NavSpan = styled.div`
+  color: var(--color-primary-black);
+  font-size: var(--font-size-sm);
+  margin-left: 30px;
+  cursor: pointer;
+`;
+export const S_NavLinkIconBox = styled.div`
+  ${RowFlex}
+  align-items: center;
+  position: relative;
+
+  &:hover {
+    .triangleDown-icon {
+      color: var(--color-primary-black);
+    }
+  }
+  .user-icon {
+    font-size: 26px;
+    color: var(--color-primary-black);
+  }
+  .triangleDown-icon {
+    font-size: 20px;
+    color: #868383;
+    transition: all 0.125s ease-in 0s;
   }
 `;
--- client/src/components/common/Header/Header.tsx
+++ client/src/components/common/Header/Header.tsx
@@ -1,36 +1,62 @@
-import {
-  Container,
-  ContainerFlex,
-  ContainerWrap,
-} from '../../../styles/Layout';
-import SearchBar from '../SearchBar/SearchBar';
+import { useState } from 'react';
+import { BiImageAdd } from 'react-icons/bi';
+import { FaUserCircle } from 'react-icons/fa';
+import { RxTriangleDown } from 'react-icons/rx';
+import { Link } from 'react-router-dom';
 import {
   S_HeaderContainerFlex,
   S_HeaderContainerWrap,
   S_HeaderContent,
-  S_HeaderLink,
-  S_LinkBox,
-  S_Logo,
+  S_NavLink,
+  S_NavBox,
+  S_LogoH1,
   S_LogoBox,
   S_LogoPoint,
+  S_NavLinkIconBox,
+  S_NavSpan,
 } from './Header.styles';
+import HeaderModal from './HeaderModal';
 
 function Header() {
+  /*로그인 여부에 따라 상태 변경*/
+  const [isLogin, setIsLogin] = useState(false);
+  /*유저아이콘 클릭 여부에 따라 상태 변경*/
+  const [isActiveMenu, setIsActiveMenu] = useState(false);
+
   return (
     <S_HeaderContainerWrap>
       <S_HeaderContainerFlex>
         <S_HeaderContent>
           <S_LogoBox>
-            <S_Logo>
-              pho<S_LogoPoint>to</S_LogoPoint>day
-            </S_Logo>
+            <S_LogoH1>
+              <Link to="/">
+                pho<S_LogoPoint>to</S_LogoPoint>day
+              </Link>
+            </S_LogoH1>
           </S_LogoBox>
-          {/* 메인화면이 아닌 모든 곳에는 활성화, 메인화면에서는 비활성화 되어야함 */}
+          {/* 메인화면이 아닌 모든 곳에는 활성화, 메인화면에서는 비활성화 되는 로직 구현 */}
           {/* <SearchBar /> */}
-          <S_LinkBox>
-            <S_HeaderLink to="/login">Login</S_HeaderLink>
-            <S_HeaderLink to="/signup">SignUp</S_HeaderLink>
-          </S_LinkBox>
+          {isLogin ? (
+            <S_NavBox>
+              <S_NavLink to="/upload">
+                <BiImageAdd className="addImage-icon" />
+              </S_NavLink>
+              <S_NavSpan>
+                <S_NavLinkIconBox
+                  onClick={() => setIsActiveMenu(!isActiveMenu)}
+                >
+                  <FaUserCircle className="user-icon" />
+                  <RxTriangleDown className="triangleDown-icon" />
+                  {isActiveMenu && <HeaderModal />}
+                </S_NavLinkIconBox>
+              </S_NavSpan>
+            </S_NavBox>
+          ) : (
+            <S_NavBox>
+              <S_NavLink to="/login">Login</S_NavLink>
+              <S_NavLink to="/signup">SignUp</S_NavLink>
+            </S_NavBox>
+          )}
         </S_HeaderContent>
       </S_HeaderContainerFlex>
     </S_HeaderContainerWrap>
