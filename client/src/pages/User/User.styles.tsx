f0b4909614070a77ae47fddca87862d2af6f92ba
youa7878 <dkgus7123@naver.com>
2023-03-15 17:46:12 +0900

    💄 [Design] User 프로필 부분 추가 #86

--- client/src/pages/User/User.styles.tsx
+++ client/src/pages/User/User.styles.tsx
@@ -1,44 +1,43 @@
 import styled from 'styled-components';
-import { Container, Content } from '../../styles/Layout';
+import { Content } from '../../styles/Layout';
 import { Flex, RowFlex, ColFlex } from '../../styles/GlobalStyles';
 import Button from '../../components/common/Button/Button';
-
-export const S_UserPageContainer = styled(Container)`
-  background-color: red;
-`;
 
 export const S_UserSection = styled.section`
   ${RowFlex}
   margin-top: 55px;
-  background-color: green;
-  height: 50%;
 `;
 
 export const S_UserThumnailArea = styled.div`
   ${ColFlex}
   align-items: center;
   width: 20%;
-  background-color: pink;
+  border-right: 1px solid rgba(194, 194, 194, 0.5);
+  margin-right: 5%;
 `;
 
 export const S_UserProfileIamge = styled.img`
   width: 130px;
   height: 130px;
   border-radius: 50%;
-  margin: 10px 0;
 `;
 
 export const S_UserFollowContainer = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
+  margin-bottom: 15px;
+`;
+
+export const S_UserFollowCount = styled.span`
+  ${Flex}
+  font-size: var(--font-size-m);
+  margin: 8px 10px;
 `;
 
 export const S_UserFollow = styled.span`
-  display: flex;
-  justify-content: center;
   font-size: var(--font-size-sm);
-  margin: 8px 10px;
+  margin: 0 10px;
 `;
 
 export const S_Button = styled(Button)`
@@ -47,14 +46,13 @@ export const S_Button = styled(Button)`
 
 export const S_UserInfoArea = styled(Content)`
   width: 80%;
-  background-color: gold;
 `;
 
 export const S_UserPageTitle = styled.div`
-  background-color: skyblue;
   display: flex;
   align-items: center;
-  height: 10%;
+  margin: 5% 0 2% 5%;
+  height: 5%;
 `;
 
 export const S_UserPhotoContentBox = styled.article`
@@ -69,8 +67,6 @@ export const S_UserPhotoContentBox = styled.article`
 `;
 
 export const S_UserPhotoContent = styled.img`
-  background-color: yellow;
-  /* width 추후에 수정 */
   width: 100%;
   height: 200px;
   object-fit: cover;
--- client/src/pages/User/User.tsx
+++ client/src/pages/User/User.tsx
@@ -1,67 +1,103 @@
 import {
-  S_UserPageContainer,
   S_UserSection,
   S_UserThumnailArea,
   S_UserProfileIamge,
   S_UserFollowContainer,
+  S_UserFollowCount,
   S_UserFollow,
   S_UserInfoArea,
   S_UserPageTitle,
   S_UserPhotoContentBox,
   S_UserPhotoContent,
   S_Pagination,
-  S_Button,
 } from './User.styles';
-import { ContainerWrap } from '../../styles/Layout';
+import { ContainerWrap, Container } from '../../styles/Layout';
 import oceanImage from '../../assets/imgs/image9.jpg';
 import kungyaImage from '../../assets/imgs/kungyaImage.png';
+import Button from '../../components/common/Button/Button';
 
-const data = {
-  data: {
-    userId: 1,
-    name: 'test',
-    profileImageUrl:
-      'https://cdn.discordapp.com/attachments/1082610363712950272/1082610364371435540/userImage.png',
-    description: '안녕하세요!',
-    likeCount: 0,
-    reportCount: 0,
-    followerCount: 0,
-    followingCount: 0,
-    checkFollow: false,
-  },
+// 나중에 데이터 받아올 때 쓸 interface
+interface DataType {
+  data: UserData[];
+}
+
+interface UserData {
+  userId: number;
+  name: string;
+  profileImageUrl: string;
+  description: string;
+  likeCount: number;
+  reportCount: number;
+  followerCount: number;
+  followingCount: number;
+  checkFollow: boolean;
+}
+
+// 임시 데이터
+const data: DataType = {
+  data: [
+    {
+      userId: 1,
+      name: 'test',
+      profileImageUrl:
+        'https://cdn.discordapp.com/attachments/1082610363712950272/1082610364371435540/userImage.png',
+      description: '안녕하세요!',
+      likeCount: 0,
+      reportCount: 0,
+      followerCount: 0,
+      followingCount: 0,
+      checkFollow: false,
+    },
+    {
+      userId: 1,
+      name: 'test',
+      profileImageUrl:
+        'https://cdn.discordapp.com/attachments/1082610363712950272/1082610364371435540/userImage.png',
+      description: '안녕하세요!',
+      likeCount: 0,
+      reportCount: 0,
+      followerCount: 0,
+      followingCount: 0,
+      checkFollow: false,
+    },
+  ],
 };
 
 function Users() {
   return (
     <ContainerWrap>
-      <S_UserPageContainer>
+      <Container>
         <S_UserSection>
           <S_UserThumnailArea>
             <S_UserProfileIamge
               alt="user profile"
-              src={data.data.profileImageUrl}
+              src={data.data[0].profileImageUrl}
             />
             <S_UserFollowContainer>
               <div>
-                <S_UserFollow>{data.data.followerCount}</S_UserFollow>
+                <S_UserFollowCount>
+                  {data.data[0].followerCount}
+                </S_UserFollowCount>
                 <S_UserFollow>Follower</S_UserFollow>
               </div>
               <div>
-                <S_UserFollow>{data.data.followingCount}</S_UserFollow>
+                <S_UserFollowCount>
+                  {data.data[0].followingCount}
+                </S_UserFollowCount>
                 <S_UserFollow>Following</S_UserFollow>
               </div>
             </S_UserFollowContainer>
-            <S_Button variant="point" shape="default" size="small">
+            <Button variant="point" shape="default" size="large">
               Upload Image
-            </S_Button>
-            <S_Button variant="point" shape="default" size="small">
+            </Button>
+            <Button variant="point" shape="default" size="XLarge" disabled>
               Remove Image
-            </S_Button>
+            </Button>
           </S_UserThumnailArea>
           <S_UserInfoArea>user info</S_UserInfoArea>
         </S_UserSection>
         <S_UserPageTitle>
-          <h3>{`${data.data.name}'s photoday`}</h3>
+          <h3>{`${data.data[0].name}'s photoday`}</h3>
           <div>bookmark Icon</div>
         </S_UserPageTitle>
         <S_UserPhotoContentBox>
@@ -72,8 +108,9 @@ function Users() {
           <S_UserPhotoContent alt="photo" src={oceanImage} />
           <S_UserPhotoContent alt="photo" src={oceanImage} />
         </S_UserPhotoContentBox>
+        {/* 페이지 네이션 추가 후, 수정 */}
         <S_Pagination>1 2 3 4 5 6</S_Pagination>
-      </S_UserPageContainer>
+      </Container>
     </ContainerWrap>
   );
 }
