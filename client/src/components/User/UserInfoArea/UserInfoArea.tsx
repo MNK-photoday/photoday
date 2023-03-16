2586ad8a087113e74994a2a1be93a7e7c6ef2f9e
youa7878 <dkgus7123@naver.com>
2023-03-16 22:16:10 +0900

    ✨ [Feat] User page 비밀번호 유효성 검사 추가 #124

--- client/src/components/User/UserInfoArea/UserInfoArea.tsx
+++ client/src/components/User/UserInfoArea/UserInfoArea.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import {
   S_UserInfoArea,
   S_UserNameContainer,
@@ -8,26 +8,52 @@ import {
   S_TextButtonWrap,
   S_TextButton,
   S_DeleteAccountText,
+  S_InputWrap,
 } from './UserInfoArea.styles';
-import {
-  UserInfoTextarea,
-  SetPasswordInput,
-} from './UserInfoInput/UserInfoInput';
+import UserInfoTextarea from './UserInfoTextarea/UserInfoTextarea';
 import { FaHeart } from 'react-icons/fa';
-import { MdReportProblem } from 'react-icons/md';
+import { TbPhotoCancel } from 'react-icons/tb';
+import { MdHideImage } from 'react-icons/md';
 import { DataType } from '../../../pages/User/User';
+import { validationValue } from '../../Login/LoginValidationLogic/LoginValidationLogic';
+import { PasswordInput } from '../../Login/Input/Input';
+import { S_InvalidMessage } from '../../../pages/Login/Login.styles';
 
 interface IProps {
-  userdata: DataType;
+  userData: DataType;
 }
 
-function UserInfoArea({ userdata }: IProps) {
+function UserInfoArea({ userData }: IProps) {
+  const [inputValue, setInputValue] = useState('');
+  const [confirminputValue, setconfirmInputValue] = useState('');
+  const [validValue, setValidValue] = useState(true);
   const [isEdit, setIsEdit] = useState(false);
   const [isChangePassWord, setIsChangePassWord] = useState(false);
+  const valueType = 'password';
+
+  useEffect(() => {
+    validationValue({ inputValue, setValidValue, valueType });
+  }, [inputValue]);
 
   const clickEditHandler = () => {
     setIsEdit(!isEdit);
   };
+
+  const changePasswordValueHandler = (
+    e: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    const value = e.target.value;
+    setInputValue(value);
+  };
+
+  const changeConfirmPasswordHandler = (
+    e: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    const value = e.target.value;
+    setconfirmInputValue(value);
+  };
+  console.log('하고 싶은 비밀번호', inputValue);
+  console.log('확인할 비밀번호', confirminputValue);
 
   const clickChangePassWordHandler = () => {
     setIsChangePassWord(!isChangePassWord);
@@ -36,22 +62,53 @@ function UserInfoArea({ userdata }: IProps) {
   return (
     <S_UserInfoArea>
       <S_UserNameContainer>
-        <S_UserName>{userdata.data[0].name}</S_UserName>
+        <S_UserName>{userData.data[0].name}</S_UserName>
         <FaHeart size={18} className="likeicon" />
-        <S_UserLikeAndReport>{userdata.data[0].likeCount}</S_UserLikeAndReport>
-        <MdReportProblem size={20} className="reporticon" />
+        <S_UserLikeAndReport>{userData.data[0].likeCount}</S_UserLikeAndReport>
+        {/* 아이콘 고민 중 <TbPhotoCancel size={25} className="reporticon" /> */}
+        <MdHideImage size={20} className="reporticon" />
         <S_UserLikeAndReport>
-          {userdata.data[0].reportCount}
+          {userData.data[0].reportCount}
         </S_UserLikeAndReport>
       </S_UserNameContainer>
       <S_UserDescription isEdit={isEdit}>
-        {isEdit ? <UserInfoTextarea /> : userdata.data[0].description}
+        {isEdit ? <UserInfoTextarea /> : userData.data[0].description}
       </S_UserDescription>
       <S_TextButtonWrap>
         <S_TextButton isTextButtonType="edit" onClick={clickEditHandler}>
           {isEdit ? 'Save edits' : 'Edit'}
         </S_TextButton>
-        {isChangePassWord && <SetPasswordInput />}
+        {isChangePassWord && (
+          <S_InputWrap>
+            <PasswordInput
+              labelValue="New password"
+              passwordValue={inputValue}
+              changeEventHandler={changePasswordValueHandler}
+            />
+            {!validValue && (
+              <S_InvalidMessage isShowMessage={!validValue ? 'show' : 'hide'}>
+                Passwords must contain 8 to 16 characters in English, numbers,
+                and special characters.
+              </S_InvalidMessage>
+            )}
+            <PasswordInput
+              labelValue="Confirm new password"
+              passwordValue={confirminputValue}
+              changeEventHandler={changeConfirmPasswordHandler}
+            />
+            {!(inputValue === confirminputValue) && (
+              <S_InvalidMessage isShowMessage={!validValue ? 'show' : 'hide'}>
+                Passwords do not match.
+              </S_InvalidMessage>
+            )}
+            <S_TextButton
+              isTextButtonType="Cancel"
+              onClick={clickChangePassWordHandler}
+            >
+              Cancel
+            </S_TextButton>
+          </S_InputWrap>
+        )}
         <S_TextButton
           isTextButtonType="changePassword"
           onClick={clickChangePassWordHandler}
