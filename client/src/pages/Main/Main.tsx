1d90d77f93c283374fea026a415bfc09a676caff
18137036 <9wjdgh9@naver.com>
2023-03-20 11:18:13 +0900

    :lipstick: [Design] Main Skeleton 적용 #40

--- client/src/pages/Main/Main.tsx
+++ client/src/pages/Main/Main.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Container, ContainerWrap } from '../../styles/Layout';
 import MainImage from '../../assets/imgs/image1.jpg';
 import SearchBar from '../../components/common/SearchBar/SearchBar';
@@ -14,10 +14,18 @@ import {
   S_MainTitle,
   S_MainImageContentBox,
 } from './Main.styles';
+import Skeleton from '../../components/common/Skeleton/Skeleton';
+import MainSkeleton from '../../components/common/Skeleton/MainSkeleton';
 
 function Main() {
   const [activeTextBox, setActiveTextBox] = useState(true);
 
+  /*로딩상태 전역으로 관리할까 고민 중입니당*/
+  const [loading, setLoading] = useState<boolean>(true);
+  /*임시로 로딩상태 만든 코드*/
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  });
   return (
     <ContainerWrap>
       <Container>
@@ -43,18 +51,18 @@ function Main() {
             </S_MainTextBox>
           </S_SearchContentBox>
           <S_MainImageContentBox>
-            <S_MainImgBox>
-              <S_MainImg
-                src={MainImage}
-                alt="인기가 많은 첫 번째 이미지"
-              ></S_MainImg>
-            </S_MainImgBox>
-            <S_MainImgBox>
-              <S_MainImg
-                src={MainImage}
-                alt="인기가 많은 두 번째이미지"
-              ></S_MainImg>
-            </S_MainImgBox>
+            {!loading ? (
+              <>
+                <S_MainImgBox>
+                  <S_MainImg src={MainImage} alt="인기있는 이미지"></S_MainImg>
+                </S_MainImgBox>
+                <S_MainImgBox>
+                  <S_MainImg src={MainImage} alt="인기있는 이미지"></S_MainImg>
+                </S_MainImgBox>
+              </>
+            ) : (
+              <MainSkeleton count={2} />
+            )}
           </S_MainImageContentBox>
         </S_MainContentBox>
       </Container>
