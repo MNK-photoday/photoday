280a899a425e3649baee4af1df43b3feda63fa8f
youngrong <whdudfhd1127@naver.com>
2023-03-22 10:25:17 +0900

    :hammer: [Fix] 임시 비밀번호 발급 로직 유효성 검사에 맞게 수정 #103

--- server/src/main/java/com/photoday/photoday/util/TempPassword.java
+++ server/src/main/java/com/photoday/photoday/util/TempPassword.java
@@ -7,23 +7,24 @@ import java.util.Date;
 
 @Component
 public class TempPassword {
-    public String getTempPassword(){
-        char[] charSet = new char[] {
-                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-                '!', '@', '#', '$', '%', '^', '&' };
+    public String getTempPassword() {
+        String str = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&";
 
         StringBuffer sb = new StringBuffer();
         SecureRandom sr = new SecureRandom();
         sr.setSeed(new Date().getTime());
 
-        int size = (int) Math.floor(Math.random() * (20 - 8 + 1)) + 8;
+        int size = (int) Math.floor(Math.random() * (17 - 8 + 1)) + 8;
 
-        for (int i=0; i<size; i++) {
-            int idx = sr.nextInt(charSet.length);    // 강력한 난수를 발생시키기 위해 SecureRandom을 사용한다.
-            sb.append(charSet[idx]);
+        for (int i = 0; i < size; i++) {
+            int idx = sr.nextInt(str.length());    // 강력한 난수를 발생시키기 위해 SecureRandom을 사용한다.
+            sb.append(str.charAt(idx));
         }
+
+        //유효성 검사에 충족하기 위한 로직 추가
+        sb.append(str.charAt((int) (Math.random() * 10)));
+        sb.append(str.charAt((int) (Math.random() * (str.indexOf("z") - str.indexOf("A"))) + str.indexOf("A")));
+        sb.append(str.charAt((int) (Math.random() * (str.indexOf("&") - str.indexOf("!"))) + str.indexOf("!")));
 
         return sb.toString();
     }
