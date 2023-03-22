f710aa62c12032da6082bb4af4b5a49f8706eb31
youa7878 <dkgus7123@naver.com>
2023-03-22 11:08:14 +0900

    🔨 [Update] checkAuth 수정 #145

--- client/src/api/auth.ts
+++ client/src/api/auth.ts
@@ -1,31 +1,25 @@
 function checkAuth() {
-  const tokenString = localStorage.getItem('accessToken');
-  if (!tokenString) {
+  const accessToken = localStorage.getItem('accessToken');
+  if (!accessToken) {
     return false;
   }
 
-  const { token, expiresAt } = JSON.parse(tokenString);
+  const { token } = JSON.parse(accessToken);
 
-  // 토큰이 만료된 경우
-  if (new Date().getTime() > expiresAt) {
-    localStorage.removeItem('accessToken');
-    /*
+  /*
     다시 발급받을 때 수정될 코드
     try {
       const response = await axios.get(`${import.meta.env.VITE_APP_API}/api/auth/reissue`, {
         headers: {
-          Authorization: `Bearer ${token}`,
+          Authorization: token,
         },
       });
-      response.
     } catch (error) {
       console.error(error);
     }
-    */
-    return false;
-  } else {
-    return true;
-  }
+  */
+
+  return true;
 }
 
 export default checkAuth;
