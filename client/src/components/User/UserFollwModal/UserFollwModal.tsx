a0a0d70a608cef06e247e3c080e0840f9d7365c9
youa7878 <dkgus7123@naver.com>
2023-03-24 02:45:53 +0900

    🔨 [Update] 팔로우 정보 모달 구현 중 #169

--- client/src/components/User/UserFollwModal/UserFollwModal.tsx
+++ client/src/components/User/UserFollwModal/UserFollwModal.tsx
@@ -1,33 +1,69 @@
+import { useEffect, useState } from 'react';
 import {
   S_UserFollwModalContainer,
   S_UserFollwListContainer,
 } from './UserFollwModal.styles';
-import ModalListItem from './ModalListItem';
+// import ModalListItem from './ModalListItem';
 import Button from '../../common/Button/Button';
+import { getFollows } from '../../../api/User';
+
+type User = {
+  userId: number;
+  name: string;
+};
+
+export type FollowUserData = {
+  userFollowing: User[];
+  userFollower: User[];
+  userFollowingCount: number;
+  userFollowerCount: number;
+};
+
+export type FollowData = {
+  data: FollowUserData;
+};
 
 interface Modal {
   setFollowModal: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 function UserFollwModal({ setFollowModal }: Modal) {
+  const [followList, setFollowList] = useState<FollowData>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await getFollows();
+        setFollowList(response.data);
+      } catch (error) {
+        alert(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  console.log('??', followList);
+
   return (
     <S_UserFollwModalContainer>
       <S_UserFollwListContainer>
+        {/* {followList.map((follow, idx) => (
+          <ModalListItem key={idx} follow={follow} />
+        ))} */}
+        {/* <ModalListItem />
         <ModalListItem />
         <ModalListItem />
         <ModalListItem />
         <ModalListItem />
         <ModalListItem />
-        <ModalListItem />
-        <ModalListItem />
-        <ModalListItem />
+        <ModalListItem /> */}
       </S_UserFollwListContainer>
       <Button
         variant="point"
         shape="default"
         size="XXLarge"
         fullWidth
-        buttonClickEvent={() => setFollowModal(false)}
+        clickEventHandler={() => setFollowModal(false)}
       >
         Close
       </Button>
