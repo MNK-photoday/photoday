bb331988d9a45a7c1c965dd3fa7a2f7456c3aad1
youngrongcho <108881135+youngrongcho@users.noreply.github.com>
2023-03-23 20:28:58 +0900

    Update TagService.java

--- server/src/main/java/com/photoday/photoday/tag/service/TagService.java
+++ server/src/main/java/com/photoday/photoday/tag/service/TagService.java
@@ -1,13 +1,9 @@
 package com.photoday.photoday.tag.service;
 
 import com.photoday.photoday.dto.MultiResponseDto;
-import com.photoday.photoday.tag.dto.TagDto;
 import com.photoday.photoday.tag.entity.Tag;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.validation.Valid;
 
 public interface TagService {
     MultiResponseDto searchByTags(@RequestParam String tags, Pageable pageable);
