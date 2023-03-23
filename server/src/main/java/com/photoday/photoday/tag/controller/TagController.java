1561ee91a8027cbc2b477e7731bd081d86bae05f
youngrongcho <108881135+youngrongcho@users.noreply.github.com>
2023-03-23 20:26:52 +0900

    Update TagController.java

--- server/src/main/java/com/photoday/photoday/tag/controller/TagController.java
+++ server/src/main/java/com/photoday/photoday/tag/controller/TagController.java
@@ -2,18 +2,14 @@ package com.photoday.photoday.tag.controller;
 
 import com.photoday.photoday.dto.MultiResponseDto;
 import com.photoday.photoday.tag.service.TagService;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.constraints.NotNull;
 
 @RestController
 @RequestMapping("/api/tags")
