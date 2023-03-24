e256515992fb10f4d44dd5974a6cc978801a46b7
youa7878 <dkgus7123@naver.com>
2023-03-24 10:53:20 +0900

    🔥 [Remove] 불필요 코드 삭제 #169

--- client/src/pages/User/User.styles.tsx
+++ client/src/pages/User/User.styles.tsx
@@ -11,25 +11,3 @@ export const S_UserSection = styled.section`
   ${RowFlex}
   margin-top: 55px;
 `;
-
-// export const S_UserPhotoContentBox = styled.article`
-//   display: flex;
-//   justify-content: center;
-//   display: grid;
-//   grid-template-columns: 1fr 1fr 1fr;
-//   padding: 30px 8%;
-//   place-items: center;
-//   gap: 20px;
-// `;
-
-// export const S_UserPhotoContent = styled.div`
-//   width: 100%;
-//   height: 210px;
-//   object-fit: cover;
-// `;
-
-// 페이지네이션 컴포넌트 생성 후 삭제
-export const S_Pagination = styled.div`
-  ${Flex}
-  padding-bottom: 55px;
-`;
