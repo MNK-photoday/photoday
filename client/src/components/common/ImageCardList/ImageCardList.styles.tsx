b94bcd7c365da9956bc65b51a508e2eaa006505e
18137036 <9wjdgh9@naver.com>
2023-03-27 20:27:07 +0900

    :lipstick: [Design] ImageCardList 반응형 styles 추가

--- client/src/components/common/ImageCardList/ImageCardList.styles.tsx
+++ client/src/components/common/ImageCardList/ImageCardList.styles.tsx
@@ -1,26 +1,30 @@
 import styled from 'styled-components';
 
 type ImageCardBoxProps = {
-  width: number;
   height?: number;
   matrix: 'columns' | 'rows';
 };
 
-export const S_ImageCardWrap = styled.article`
-  width: 100%;
-`;
-
-export const S_ImageCardBox = styled.div<ImageCardBoxProps>`
+export const S_ImageCardWrap = styled.article<ImageCardBoxProps>`
   width: 100%;
   display: grid;
-  ${({ width, matrix }) =>
-    width &&
-    matrix &&
-    `grid-template-${matrix}: repeat(auto-fill, minmax(${width}px, 1fr))`};
+  grid-template-columns: 1fr 1fr 1fr;
   row-gap: 10px;
   column-gap: 10px;
   ${({ height }) => height && `grid-auto-rows: ${height}px`};
+
+  @media screen and (max-width: 1500px) {
+    grid-template-columns: 1fr 1fr;
+    padding: 0 50px;
+  }
+  @media screen and (max-width: 1020px) {
+    grid-template-columns: 1fr;
+    grid-auto-rows: 350px;
+    padding: 0 50px;
+  }
 `;
+
+export const S_ImageCardBox = styled.div``;
 
 export const S_LoaderBar = styled.div`
   height: 20px;
