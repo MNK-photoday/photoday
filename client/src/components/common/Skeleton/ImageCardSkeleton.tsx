7483d6645655fcd02751f7949d75aa9245116d17
18137036 <9wjdgh9@naver.com>
2023-03-27 20:29:55 +0900

    :lipstick: [Design] ImageCardSkeleton styles 수정

--- client/src/components/common/Skeleton/ImageCardSkeleton.tsx
+++ client/src/components/common/Skeleton/ImageCardSkeleton.tsx
@@ -1,18 +1,17 @@
-import { S_ImageCardBox } from '../ImageCardList/ImageCardList.styles';
+import { S_ImageCardWrap } from '../ImageCardList/ImageCardList.styles';
 import Skeleton from './Skeleton';
 type ImageCardSkeletonProps = {
-  width: number;
   height?: number;
   count: number;
 };
 
-function ImageCardSkeleton({ count, width, height }: ImageCardSkeletonProps) {
+function ImageCardSkeleton({ count, height }: ImageCardSkeletonProps) {
   return (
-    <S_ImageCardBox width={width} height={height} matrix="columns">
+    <S_ImageCardWrap height={height} matrix="columns">
       {Array.from({ length: count }).map((_, index) => (
         <Skeleton key={index} height={height} animation={true} />
       ))}
-    </S_ImageCardBox>
+    </S_ImageCardWrap>
   );
 }
 
