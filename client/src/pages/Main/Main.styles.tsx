6dd4244e0fcff46142122ee8e7917245f65cc2ff
18137036 <9wjdgh9@naver.com>
2023-03-27 20:31:17 +0900

    :lipstick: [Design] Main styles 수정

--- client/src/pages/Main/Main.styles.tsx
+++ client/src/pages/Main/Main.styles.tsx
@@ -1,17 +1,10 @@
 import styled from 'styled-components';
-import { RowFlex } from '../../styles/GlobalStyles';
 
 export const S_MainContentBox = styled.div`
-  ${RowFlex}
-  height: 100%;
-  padding: 40px 0;
-  box-sizing: border-box;
-  align-items: center;
+  display: flex;
   width: 100%;
-  justify-content: space-around;
-  padding: 30px;
   height: calc(100vh - 70px);
-
+  align-items: center;
   @media screen and (max-width: 1024px) {
     flex-wrap: wrap;
     width: 100%;
@@ -24,7 +17,7 @@ export const S_SearchContentBox = styled.section`
   width: 50%;
   height: 450px;
   position: relative;
-  padding: 30px 0px;
+  padding: 30px;
 
   @media screen and (max-width: 1024px) {
     width: 100%;
