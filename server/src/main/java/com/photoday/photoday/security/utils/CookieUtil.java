5bac4909cf01503318c580a79a7e9e97e932c74c
ichubtou <msjj0809@naver.com>
2023-03-27 21:40:43 +0900

    :hammer: [Fix] test

--- server/src/main/java/com/photoday/photoday/security/utils/CookieUtil.java
+++ server/src/main/java/com/photoday/photoday/security/utils/CookieUtil.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 public class CookieUtil {
     static public Cookie createCookie(String cookieName, String value) {
         Cookie cookie = new Cookie(cookieName, value);
-        cookie.setDomain("localhost"); //TODO 프론트 기준으로 추후 변경
+        cookie.setDomain("photodayproject.s3-website.ap-northeast-2.amazonaws.com");
         cookie.setHttpOnly(true);
         cookie.setMaxAge((int) (1000L * 3600 * 24 * 30));
         cookie.setPath("/");
