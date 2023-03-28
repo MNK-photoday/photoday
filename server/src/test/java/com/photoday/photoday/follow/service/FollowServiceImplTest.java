cba47f7a79e67fcbba84e44d98b56d66ab9f48dd
qushe8r <qushe8r@gmail.com>
2023-03-28 15:19:59 +0900

    :recycle: [Refactor] FollowServiceImplTest: SpringBootTest -> Mock Test #203

--- server/src/test/java/com/photoday/photoday/follow/service/FollowServiceImplTest.java
+++ server/src/test/java/com/photoday/photoday/follow/service/FollowServiceImplTest.java
@@ -1,88 +1,110 @@
 package com.photoday.photoday.follow.service;
 
-import com.photoday.photoday.follow.dto.FollowDto;
+import com.photoday.photoday.excpetion.CustomException;
 import com.photoday.photoday.follow.entity.Follow;
+import com.photoday.photoday.follow.mapper.FollowMapper;
 import com.photoday.photoday.follow.repository.FollowRepository;
 import com.photoday.photoday.follow.service.impl.FollowServiceImpl;
 import com.photoday.photoday.security.service.AuthUserService;
 import com.photoday.photoday.user.entity.User;
-import com.photoday.photoday.user.repository.UserRepository;
-import org.junit.jupiter.api.BeforeEach;
+import com.photoday.photoday.user.service.UserService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.Objects;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
 class FollowServiceImplTest {
-    @Autowired
-    FollowServiceImpl followService;
-    @Autowired
-    UserRepository userRepository;
-    @Autowired
+    @Mock
+    UserService userService;
+    @Mock
     FollowRepository followRepository;
-    @MockBean
+    @Spy
+    FollowMapper followMapper;
+    @Mock
     AuthUserService authUserService;
+    @InjectMocks
+    FollowServiceImpl followService;
 
-    @BeforeEach
-    void clear() {
-        userRepository.deleteAll();
-        followRepository.deleteAll();
-    }
+    private Long userId = 0L;
 
     @Test
     void findFollowUser() {
         // given
         User following = getUser("following@email.com", "following");
         User follower = getUser("follower@email.com", "follower");
-
-        Follow follow = new Follow();
-        follow.setFollowing(following);
-        follow.setFollower(follower);
-        followRepository.save(follow);
-
-        given(authUserService.getLoginUserId()).willReturn(null);
+        given(authUserService.getLoginUserId()).willReturn(following.getUserId());
+        given(userService.findVerifiedUser(anyLong())).willReturn(follower);
 
         // when
-        FollowDto.ResponseFollowUsers followingResponse = followService.findFollowUser(following.getUserId());
-        FollowDto.ResponseFollowUsers followerResponse = followService.findFollowUser(follower.getUserId());
-
-        // then
-        assertEquals(1, followingResponse.getUserFollowingCount());
-        assertEquals(0, followingResponse.getUserFollowerCount());
-        assertEquals(0, followerResponse.getUserFollowingCount());
-        assertEquals(1, followerResponse.getUserFollowerCount());
-        assertFalse(followerResponse.getUserFollower().get(0).isCheckFollow());
-        assertTrue(followingResponse.getUserFollowing().stream().anyMatch(followerUser -> Objects.equals(followerUser.getUserId(), follower.getUserId())));
+        followService.findFollowUser(follower.getUserId());
     }
 
     @Test
-    void registerFollowUser() {
+    void registerFollowUserSave() {
+        // given
         User following = getUser("following@email.com", "following");
         User follower = getUser("follower@email.com", "follower");
+        Follow follow = new Follow();
+        follow.setFollowId(1L);
+        follow.setFollowing(following);
+        follow.setFollower(follower);
+        given(authUserService.getLoginUser()).willReturn(Optional.of(following));
+        given(userService.findVerifiedUser(anyLong())).willReturn(follower);
+        given(followRepository.findByFollowerAndFollowing(any(User.class), any(User.class))).willReturn(Optional.empty());
+        given(followRepository.save(any(Follow.class))).willReturn(follow);
 
-        given(authUserService.getLoginUserId()).willReturn(following.getUserId());
+        // when
+        followService.registerFollowUser(follower.getUserId());
+    }
 
-        FollowDto.ResponseFollowUsers response = followService.registerFollowUser(follower.getUserId());
+    @Test
+    void registerFollowUserCannotFollowMyself() {
+        // given
+        User following = getUser("following@email.com", "following");
+        given(authUserService.getLoginUser()).willReturn(Optional.of(following));
 
-        assertEquals(following.getUserId(), response.getUserFollower().get(0).getUserId());
-        assertEquals(following.getName(), response.getUserFollower().get(0).getName());
-        assertTrue(response.getUserFollowing().isEmpty());
-        assertEquals(1L, response.getUserFollowerCount());
-        assertEquals(0L, response.getUserFollowingCount());
+        // when
+        assertThrows(CustomException.class, () -> followService.registerFollowUser(following.getUserId()));
+    }
+
+    @Test
+    void registerFollowUserRemove() {
+        // given
+        User following = getUser("following@email.com", "following");
+        User follower = getUser("follower@email.com", "follower");
+        Follow follow = new Follow();
+        follow.setFollowId(1L);
+        follow.setFollowing(following);
+        follow.setFollower(follower);
+        given(authUserService.getLoginUser()).willReturn(Optional.of(following));
+        given(userService.findVerifiedUser(anyLong())).willReturn(follower);
+        given(followRepository.findByFollowerAndFollowing(any(User.class), any(User.class))).willReturn(Optional.of(follow));
+
+        // when
+        followService.registerFollowUser(follower.getUserId());
     }
 
     private User getUser(String email, String owner) {
-        User user = User.builder()
+        return User.builder()
+                .userId(getId())
                 .email(email)
                 .name(owner)
                 .password("123456a!")
                 .build();
-        return userRepository.save(user);
+    }
+
+    private Long getId() {
+        userId += 1L;
+        return userId;
     }
 }
