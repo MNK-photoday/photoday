dec28a4feb53a0de76a65da554162931e2a744d3
18137036 <9wjdgh9@naver.com>
2023-03-29 22:46:12 +0900

    :hammer: [Update] Image API 요청 에러 핸들링 위치 수정

--- client/src/api/Image.ts
+++ client/src/api/Image.ts
@@ -1,79 +1,51 @@
-import axios, { AxiosError, AxiosResponse } from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
 export const patchLikeImage = async (imageId: number) => {
   const jwtToken = localStorage.getItem('accessToken');
-  try {
-    const response: AxiosResponse = await axios.patch(
-      `${import.meta.env.VITE_APP_API}/images/${imageId}/likes`,
-      {},
-      {
-        headers: {
-          Authorization: `${jwtToken}`,
-        },
+  const response: AxiosResponse = await axios.patch(
+    `${import.meta.env.VITE_APP_API}/images/${imageId}/likes`,
+    {},
+    {
+      headers: {
+        Authorization: `${jwtToken}`,
       },
-    );
-    return response.data;
-  } catch (error) {
-    if (error instanceof AxiosError) {
-      if (error.response?.status === 401) {
-        alert('로그인 후 이용 가능합니다. ');
-      }
-      console.log(error.response);
-    }
-  }
+    },
+  );
+  return response.data;
 };
 
 export const patchBookmarkImage = async (imageId: number) => {
   const jwtToken = localStorage.getItem('accessToken');
-  try {
-    const response: AxiosResponse = await axios.patch(
-      `${import.meta.env.VITE_APP_API}/images/${imageId}/bookmarks`,
-      {},
-      {
-        headers: {
-          Authorization: `${jwtToken}`,
-        },
+
+  const response: AxiosResponse = await axios.patch(
+    `${import.meta.env.VITE_APP_API}/images/${imageId}/bookmarks`,
+    {},
+    {
+      headers: {
+        Authorization: `${jwtToken}`,
       },
-    );
-    return response.data;
-  } catch (error) {
-    if (error instanceof AxiosError) {
-      console.log(error.response);
-    }
-  }
+    },
+  );
+  return response.data;
 };
 
 export const getDetailImage = async (imageId: number) => {
   const jwtToken = localStorage.getItem('accessToken');
-  try {
-    const response: AxiosResponse = await axios.get(
-      `${import.meta.env.VITE_APP_API}/images/${imageId}`,
-      {
-        headers: {
-          Authorization: `${jwtToken}`,
-        },
+
+  const response: AxiosResponse = await axios.get(
+    `${import.meta.env.VITE_APP_API}/images/${imageId}`,
+    {
+      headers: {
+        Authorization: `${jwtToken}`,
       },
-    );
-    return response.data;
-  } catch (error) {
-    if (error instanceof AxiosError) {
-      if (error.response?.status === 401) {
-        alert('로그인 후 이용 가능합니다. ');
-      }
-      console.log(error.response);
-    }
-  }
+    },
+  );
+  return response.data;
 };
 
 export const getMainImage = async () => {
-  try {
-    const response: AxiosResponse = await axios.get(
-      `${import.meta.env.VITE_APP_API}/images/main`,
-    );
-    return response.data;
-  } catch (error) {
-    if (error instanceof AxiosError) {
-      console.log(error.response);
-    }
-  }
+  const response: AxiosResponse = await axios.get(
+    `${import.meta.env.VITE_APP_API}/images/main`,
+  );
+  return response.data;
 };
