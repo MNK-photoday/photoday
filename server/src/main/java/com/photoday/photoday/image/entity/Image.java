aa36a95f7469bc050df6b55a037c5c7c0b163271
youngrong <whdudfhd1127@naver.com>
2023-05-04 18:52:56 +0900

    :sparkles: [Feat] image entity resizeUrl 필드 추가 #308

--- server/src/main/java/com/photoday/photoday/image/entity/Image.java
+++ server/src/main/java/com/photoday/photoday/image/entity/Image.java
@@ -26,6 +26,9 @@ public class Image {
     @Column(nullable = false, columnDefinition = "LONGTEXT")
     private String imageUrl;
 
+    @Column(columnDefinition = "LONGTEXT")
+    private String resizedUrl;
+
     @Column(nullable = false)
     private int viewCount;
 
