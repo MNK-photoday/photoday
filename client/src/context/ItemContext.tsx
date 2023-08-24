71e90cd1e4675569eff91276c4d643538ac48377
leejungho9 <9wjdgh9@naver.com>
2023-08-24 14:15:19 +0900

    :hammer: [Update] 타입명 변경

--- client/src/context/ItemContext.tsx
+++ client/src/context/ItemContext.tsx
@@ -1,14 +1,14 @@
 import React, { PropsWithChildren, useState } from 'react';
-import { ImageItemProps } from '../components/common/ImageCardList/ImageCardList';
+import { ImageCardProps } from '../components/common/ImageCardList/ImageCardList';
 
 type ItemContextProps = {
-  items: ImageItemProps[];
-  setItems: React.Dispatch<React.SetStateAction<ImageItemProps[]>>;
+  items: ImageCardProps[];
+  setItems: React.Dispatch<React.SetStateAction<ImageCardProps[]>>;
 };
 export const ItemContext = React.createContext<ItemContextProps | null>(null);
 
 const ItemStore = ({ children }: PropsWithChildren<{}>) => {
-  const [items, setItems] = useState<ImageItemProps[]>([]);
+  const [items, setItems] = useState<ImageCardProps[]>([]);
   return (
     <ItemContext.Provider value={{ items, setItems }}>
       {children}
